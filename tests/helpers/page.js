cdd2d8337870eab30ab76e7f78d0c3dcba0eaf43
Michal Stepien <michalstepien@MacBook-Pro-Michal.local>
2021-04-12 15:55:09 +0200

    final test setup

--- tests/helpers/page.js
+++ tests/helpers/page.js
@@ -1,4 +1,6 @@
 const puppeteer = require('puppeteer');
+const userFactory = require('../factories/userFactory');
+const sessionFactory = require('../factories/sessionFactory');
 
 class CustomPage {
   static async build() {
@@ -10,15 +12,28 @@ class CustomPage {
 
     return new Proxy(customPage, {
       get: function (target, property) {
-        return target[property] || page[property] || browser[property];
+        return target[property] || browser[property] || page[property];
       },
     });
   }
 
-  constructor() {
+  constructor(page) {
     this.page = page;
+  }
+
+  async login() {
+    const user = await userFactory();
+    const { session, sig } = sessionFactory(user);
+
+    await this.page.setCookie({ name: 'session', value: session });
+    await this.page.setCookie({ name: 'session.sig', value: sig });
+    await this.page.goto('localhost:3000/blogs');
+    await this.page.waitFor('a[href="/auth/logout"]');
+  }
+
+  async getContentsOf(selector) {
+    return this.page.$eval(selector, (el) => el.innerHTML);
   }
 }
 
 module.exports = CustomPage;
-// CustomPage.build();
